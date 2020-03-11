9a60d17bc6b757c00abac17e6f633735c83c9955
957859795@qq.com <dyqwaszx132>
2020-03-11 11:10:23 +0800

    修改allure文件的代码注释

--- testcase/t_allure/allure_demo.py
+++ testcase/t_allure/allure_demo.py
@@ -5,23 +5,23 @@ import allure
 import  os
 @allure.feature("类级别标签")
 class TestAllure:
-    # @allure.title("用例1")
-    # @allure.description("我是备注执行测试用例1的结果是成功")
-    # @allure.story("方法级别标签1")
-    # def test_1(self):
-    #     print("test_1")
-    #
-    # @allure.title("用例2")
-    # @allure.description("我是备注执行测试用例2的结果是成功")
-    # @allure.story("方法级别标签2")
-    # def test_2(self):
-    #     print("test_2")
-    #
-    # @allure.title("用例3")
-    # @allure.description("我是备注执行测试用例3的结果是成功")
-    # @allure.story("方法级别标签3")
-    # def test_3(self):
-    #     print("test_3")
+    @allure.title("用例1")
+    @allure.description("我是备注执行测试用例1的结果是成功")
+    @allure.story("方法级别标签1")
+    def test_1(self):
+        print("test_1")
+
+    @allure.title("用例2")
+    @allure.description("我是备注执行测试用例2的结果是成功")
+    @allure.story("方法级别标签2")
+    def test_2(self):
+        print("test_2")
+
+    @allure.title("用例3")
+    @allure.description("我是备注执行测试用例3的结果是成功")
+    @allure.story("方法级别标签3")
+    def test_3(self):
+        print("test_3")
     @pytest.mark.parametrize("case",["csae1","case2"])
     def test_4(self,case):
         print(case)
